f7981e2f336f426112b1397eee6904b552731b3c
John Walley <john@walley.org.uk>
2016-09-03 11:41:41 +0100

    Get dependencies on a sure footing (#1)
    * Do not roll dependencies into build artifacts
    
    * Bump version

--- test/bumpsChart-test.js
+++ test/bumpsChart-test.js
@@ -2,6 +2,6 @@ var tape = require("tape"),
     bumpsChart = require("../");
 
 tape("setup() a valid svg element", function(test) {
-  test.doesNotThrow(bumpsChart.bumpsChart({}).setup, "");
+  test.doesNotThrow(bumpsChart.bumpsChart().setup, "");
   test.end();
 });
